a15812c11a19057b13760146159734d626488a98
DanieChoi <swchoi317@kakao.com>
2025-03-20 15:41:49 +0900

    [최상원]JPA 설정변경으로 인한 컬럼 지정

--- src/main/java/com/nexus/pdsw/entity/EventLogEntity.java
+++ src/main/java/com/nexus/pdsw/entity/EventLogEntity.java
@@ -18,6 +18,7 @@ import java.time.LocalDateTime;
 import com.nexus.pdsw.common.AesEncDecConverter;
 import com.nexus.pdsw.dto.request.PostEventLogRequestDto;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -36,24 +37,42 @@ import lombok.NoArgsConstructor;
 public class EventLogEntity {
   
   @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name = "uidx", nullable = false)
   private Long uidx;                //일련번호
+  @Column(name = "tenant_id", nullable = false)
   private int tenantId;             //테넌트ID
+  @Column(name = "employee_id", nullable = false)
   private String employeeId;        //상담원ID
+  @Column(name = "user_host", nullable = false)
   private String userHost;          //접속ip, hostname등
+  @Column(name = "query_id", nullable = false)
   private String queryId;           //web query id
+  @Column(name = "query_type", nullable = false)
   private String queryType;         //이벤트타입 I:insert, U:update, D:delete, R:select, A:자원변경
+  @Column(name = "activation", nullable = false)
   private String activation;        //행위 로그
   @Convert(converter = AesEncDecConverter.class)
+  @Column(name = "description", nullable = true)
   private String description;       //부가정보
+  @Column(name = "success_flag", nullable = true)
   private int successFlag;          //성공여부 1 : 성공 0 : 실패 또는 DB 에러코드
+  @Column(name = "event_time", nullable = false)
   private LocalDateTime eventTime;  //이벤트일시
+  @Column(name = "event_name", nullable = true)
   private String eventName;         //이벤트명 login, logout, 테이블명, employee.postion_level
+  @Column(name = "query_rows", nullable = false)
   private int queryRows;            //조회건수
+  @Column(name = "target_id", nullable = true)
   private String targetId;          //대상자ID
+  @Column(name = "user_session_type", nullable = true)
   private int userSessionType;      //사용자접속타입 0 : 로컬PC 1 : VDI 또는 재택 연결 시
+  @Column(name = "export_flag", nullable = true)
   private int exportFlag;           //export실행여부 1 : export 실행
+  @Column(name = "memo", nullable = true)
   private String memo;              //메모 ex)파일다운사유 등
+  @Column(name = "update_employee_id", nullable = true)
   private String updateEmployeeId;  //수정상담원ID
+  @Column(name = "update_time", nullable = true)
   private LocalDateTime updateTime; //수정일시
 
 	/*
