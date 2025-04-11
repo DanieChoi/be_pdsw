66b4d47bdfca80e417b8c645479ccb676351656d
DanieChoi <swchoi317@kakao.com>
2025-04-11 09:21:53 +0900

    [최상원]CI/CD 테스트

--- src/main/java/com/nexus/pdsw/service/impl/CounselorServiceImpl.java
+++ src/main/java/com/nexus/pdsw/service/impl/CounselorServiceImpl.java
@@ -165,8 +165,8 @@ public class CounselorServiceImpl implements CounselorService {
 
       //사이드 바(캠페인)에서 캠페인 노드에서 상담원 상태 모니터를 호출했을 때
       if (!requestBody.getCampaignId().equals("0")) {
-        log.info(">>>테넌트ID={}", requestBody.getTenantId());
-        log.info(">>>캠페인ID={}", requestBody.getCampaignId());
+        // log.info(">>>테넌트ID={}", requestBody.getTenantId());
+        // log.info(">>>캠페인ID={}", requestBody.getCampaignId());
         arrCampaignId[0] = Integer.parseInt(requestBody.getCampaignId());
 
         filterMap.put("campaign_id", arrCampaignId);
