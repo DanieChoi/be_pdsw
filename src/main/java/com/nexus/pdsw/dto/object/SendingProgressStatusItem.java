cbcb1184346f35a8e63166700d5a365e2c38d8a3
DanieChoi <swchoi317@kakao.com>
2025-04-23 13:40:45 +0900

    [최상원]발신진행상태 디버그 추가

--- src/main/java/com/nexus/pdsw/dto/object/SendingProgressStatusItem.java
+++ src/main/java/com/nexus/pdsw/dto/object/SendingProgressStatusItem.java
@@ -19,7 +19,9 @@ import java.util.Map;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Getter
 @NoArgsConstructor
 public class SendingProgressStatusItem {
@@ -45,6 +47,8 @@ public class SendingProgressStatusItem {
   private SendingProgressStatusItem(
     Map<String, Object> mapSendingProgressStatus
   ) {
+
+    log.info("mapSendingProgressStatus: {}", mapSendingProgressStatus.toString());
 
     this.campaignId = (int) mapSendingProgressStatus.get("campaign_id");
     this.campaignName = (String) mapSendingProgressStatus.get("campaign_name");
